3c94ae85bc80222461790809efae9ace892b0294
Offman fernando Eguez catalano <fernando@Mac-mini-de-Offman.local>
2026-01-21 13:57:05 -0400

    Fix import for getEtiquetasPorEntrega in etiquetas routes

--- src/routes/etiquetas.routes.js
+++ src/routes/etiquetas.routes.js
@@ -1,6 +1,7 @@
 // src/routes/etiquetas.routes.js
 import express from 'express';
-import getEtiquetasPorEntrega from '../controllers/etiquetas.controller.js';
+import { getEtiquetasPorEntrega } from '../controllers/etiquetas.controller.js';
+
 
 const router = express.Router();
 
